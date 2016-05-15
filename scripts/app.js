d3019ea57f4d0541b0428d0e32a72bd8ff6353c2
ericgold <egold05@gmail.com>
2016-05-15 14:42:08 -0700

    playpause + mute buttons
    removed console logs
    added toggling for mute button
    added icon swap for play pause
    added icon swap for mute
    removed progress-bar span
    removed class “controls” from #video-controls, adjusted styles
    verified play pause and mute work in FF, Safari, Chrome

--- scripts/app.js
+++ scripts/app.js
@@ -1,25 +1,57 @@
 var video = document.getElementById('video');
 var videoControls = document.getElementById('video-controls');
 
+var progress = document.getElementById('progress');
 var playpause = document.getElementById('playpause');
 var volume = document.getElementById('volume');
-var progress = document.getElementById('progress');
-var progressBar = document.getElementById('progress-bar');
 var fullscreen = document.getElementById('fullscreen');
 
+
+// *** PLAYPAUSE BUTTON ***
+
+//styles playpause button to pause state
+function pauseIcon() {
+	playpause.style.width = "18px";
+	playpause.style.height = "24px";
+	playpause.style.background = "url('./icons/pause-icon.png')";
+}
+
+//styles playpause button to play state
+function playIcon() {
+	playpause.style.width = "22px";
+	playpause.style.height = "26px";
+	playpause.style.background = "url('./icons/play-icon.png')";
+}
+
+//playpause button functionality
 function playPause() {
 	if (video.paused) {
-		console.log("play");
 		video.play();
+		pauseIcon();
 	} else {
-		console.log("pause");
 		video.pause();
+		playIcon();
 	}
 }
 
+// *** VOLUME BUTTON ***
+
+function onIcon() {
+	volume.style.background = "url('./icons/volume-on-icon.png')";
+}
+
+function offIcon() {
+	volume.style.background = "url('./icons/volume-off-icon.png')";
+}
+
 function muteVideo() {
-	console.log("mute");
-	video.muted();
+	if (!video.muted) {
+		video.muted = true;
+		offIcon();
+	} else {
+		video.muted = false;
+		onIcon();
+	}
 }
 
 playpause.addEventListener("click", playPause);
